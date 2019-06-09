8bcff2addcedb5b2486698434a0fab63b824ff78
dimitri <dimitri.shkoklev@hotmail.com>
2019-06-09 11:45:40 +0200

    donation accept added

--- src/main/java/com/mk/donations/repository/DonationRepository.java
+++ src/main/java/com/mk/donations/repository/DonationRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DonationRepository extends JpaRepository<Donation, Long> {
 
     List<Donation> findByOrganization_IdAndStatus(Long organizationId, String status);
+
+    Optional<Donation> findByOrganization_IdAndDemand_IdAndDonorId(Long organizationId, Long demandId, Long donorId);
 }
